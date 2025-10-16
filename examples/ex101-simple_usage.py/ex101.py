9d797d6bbdf499fec1a9ebc93f3beb4f54c1e381
N. Papadakis (d-g5-5590) <npapnet@gmail.com>
2025-10-16 20:20:25 +0300

    corrected pyproject toml to contain be able to sintall directly with the version number throught the first isntallation.

--- examples/ex101-simple_usage.py/ex101.py
+++ examples/ex101-simple_usage.py/ex101.py
@@ -10,7 +10,7 @@ This HAS NOT BEEN TESTED YET
 import numpy as np
 import matplotlib.pyplot as plt
 
-from  pymfoil import mfoil
+from  pymfoil.mfoil import mfoil
 
 
 #%%[code]
@@ -26,7 +26,7 @@ print('NACA geom name =', m.geom.name, '  num. panels =', m.foil.N)
 # add camber
 #m.geom_addcamber(np.array([[0,0.3,0.7,1],[0,-.03,.01,0]]))
 # set up a compressible viscous run (note, alpha is in degrees)
-m.setoper(alpha=25, Re=5*10**6, Ma=0.0)
+m.setoper(alpha=2.5, Re=5*10**6, Ma=0.0)
 # request plotting, specify the output file for the plot
 m.param.doplot, m.post.rfile = True, 'results.pdf'
 # set the verbosity (higher -> more output to stdout)
@@ -40,3 +40,5 @@ m.solve()
 plt.show()
 
 
+
+# %%
